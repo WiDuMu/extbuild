09f2591eb19a956a827d418b9e6e4a3047f7dbb3
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2026-01-01 21:26:04 -0600

    build script changes

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,44 +1,52 @@
 import * as Bun from "bun";
 import { cp, mkdir, readdir, rm } from "node:fs/promises";
-import * as Path from "node:path";
-import * as fflate from 'fflate';
 
-const deletePromise = rm("./build", {recursive: true});
-
+const buildDir = "./build";
+const distDir = "./dist";
 const srcDir = "./src";
 const staticDir = `${srcDir}/static`;
 
-const getEntrypoints = () => {
-    const globber = new Bun.Glob("**/**.{html,js,jsx,ts,tsx}");
-    const scan = globber.scanSync({ cwd: srcDir });
-    const entrypoints = [...scan].map(path => `./src/${path}`);
-    return entrypoints;
+async function clearDirs() {
+  const build = rm(buildDir, {recursive: true});
+  const dist = rm(distDir, {recursive: true});
+  return await Promise.all([build, dist]);
 }
 
-console.log(getEntrypoints())
+async function makeDirs() {
+  const build = mkdir(buildDir);
+  const dist = mkdir(distDir);
+  return await Promise.all([build, dist]);
+}
 
-await deletePromise;
+function getEntrypoints() {
+  const globber = new Bun.Glob("**/**.{html,js,jsx,ts,tsx}");
+  const scan = globber.scanSync({ cwd: srcDir });
+  const entrypoints = [...scan].map(path => `${srcDir}/${path}`);
+  return entrypoints;
+}
 
-const artifacts = await Bun.build({
-    banner: '"use strict"',
-    entrypoints: getEntrypoints(),
-    outdir: "./build"
+/** When you don't care about the result of an operation */
+function dontCare(promise: Promise<any>): Promise<void> {
+  return new Promise((resolve, _) => {
+    promise.then(() => resolve()).catch(() => resolve());
+  })
+}
+
+await dontCare(clearDirs());
+await makeDirs();
+
+const artifacts = Bun.build({
+  entrypoints: getEntrypoints(),
+  env: "disable",
+  format: "esm",
+  target: "browser",
+  outdir: buildDir
 });
 
-const zipped = fflate.zipSync({
+const manifestPromise = cp(`${srcDir}/manifest.json`, `${buildDir}/manifest.json`);
+const copyPromise = cp(staticDir, buildDir, { recursive: true });
 
-});
+await Promise.all([manifestPromise, copyPromise, artifacts]);
 
-const manifestFile = Bun.file("./src/manifest.json");
-await Bun.write("./build/manifest.json", manifestFile);
-
-const copyPromise = await cp(srcDir, "./build/", { recursive: true });
-
-const buildDir = await readdir("./build", { recursive: true });
-
-await Bun.$`zip -r -9 dist/ext.zip build/`;
-
-console.log(await buildDir);
-
-console.log( await artifacts);
+await Bun.$`zip -r -9 ${distDir}/ext.zip build/`;
 
